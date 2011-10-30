b4b7fbb3cfeb260ca5b2a12e47ca466caa8550ff
Ivan Shubin <ivan.ishubin@gmail.com>
2011-10-30 20:53:25 +0100

    .

--- src/main/java/net/mindengine/jeremy/registry/Lookup.java
+++ src/main/java/net/mindengine/jeremy/registry/Lookup.java
@@ -1,6 +1,8 @@
 package net.mindengine.jeremy.registry;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.mindengine.jeremy.Remote;
 import net.mindengine.jeremy.client.Client;
@@ -16,6 +18,7 @@ public class Lookup {
     private String url;
     private Client client;
     private RequestResponseHandler requestResponseHandler; 
+    private Map<String, Map<Class<?>, Object>> cashedRemoteObjects = new HashMap<String, Map<Class<?>, Object>>();
     
     public Lookup() {
     }
@@ -33,10 +36,33 @@ public class Lookup {
         return url;
     }
     
+    
+    private Object getObjectFromCache(String objectName, Class<?> interfaceClass) {
+        Map<Class<?>,Object> map = cashedRemoteObjects.get(objectName);
+        if(map!=null) {
+            return map.get(interfaceClass);
+        }
+        return null;
+    }
+    
+    private void putObjectToCache(String objectName, Class<?>interfaceClass, Object object) {
+        Map<Class<?>,Object> map = cashedRemoteObjects.get(objectName);
+        if(map==null) {
+            map = new HashMap<Class<?>, Object>();
+            cashedRemoteObjects.put(objectName, map);
+        }
+        map.put(interfaceClass, object);
+    }
 
     @SuppressWarnings("unchecked")
     public <T> T getRemoteObject(String objectName, Class<T> interfaceClass) throws RemoteObjectIsNotFoundException, ConnectionError{
-        //TODO cache all proxy objects in memory
+        
+        //Checking if there is already such object in cache
+        Object objectFromCache = getObjectFromCache(objectName, interfaceClass);
+        if(objectFromCache!=null) {
+            return (T) objectFromCache;
+        }
+        
         if(client==null) {
             client = new Client();
         }
@@ -54,7 +80,7 @@ public class Lookup {
                 
                 String[] remoteMethods = (String[]) requestResponseHandler.deserializeObject(httpResponse.getContent(), String[].class);
                 
-                Method[] declaredMethods =  interfaceClass.getMethods();
+                Method[] declaredMethods = interfaceClass.getMethods();
                 for(Method method : declaredMethods) {
                     boolean found = false;
                     for(int i=0;i<remoteMethods.length && !found; i++) {
@@ -66,7 +92,13 @@ public class Lookup {
                         throw new RemoteObjectIsNotFoundException("Remote object '"+objectName+"' doesn't support specified interface "+interfaceClass.getName()+". Remote object doesn't support '"+method.getName()+"' method");
                     }
                 }
-                return (T)ObjectInvocationHandler.createProxyRemoteObject(url, objectName, interfaceClass, client, requestResponseHandler);
+                
+                T object =  (T)ObjectInvocationHandler.createProxyRemoteObject(url, objectName, interfaceClass, client, requestResponseHandler);
+                /**
+                 * Putting just created proxy object to cache, so next time it will not be generated again
+                 */
+                putObjectToCache(objectName, interfaceClass, object);
+                return object;
             }
             else if(httpResponse.getStatus()==404) {
                 throw new RemoteObjectIsNotFoundException("There is no remote object with name '"+objectName+"'");
