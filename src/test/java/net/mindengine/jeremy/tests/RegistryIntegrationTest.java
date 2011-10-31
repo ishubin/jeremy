66c129863e54e4f9ecadf00a3fbfe07cb26a2790
Ivan Shubin <ivan.ishubin@gmail.com>
2011-10-31 14:01:16 +0100

    Added integration test for complex object sending along with remote method invocation

--- src/test/java/net/mindengine/jeremy/tests/RegistryIntegrationTest.java
+++ src/test/java/net/mindengine/jeremy/tests/RegistryIntegrationTest.java
@@ -10,8 +10,11 @@ import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.sql.rowset.serial.SerialArray;
 
 import net.mindengine.jeremy.client.Client;
 import net.mindengine.jeremy.client.HttpResponse;
@@ -19,6 +22,7 @@ import net.mindengine.jeremy.exceptions.ConnectionError;
 import net.mindengine.jeremy.exceptions.RemoteObjectIsNotFoundException;
 import net.mindengine.jeremy.objects.MyObject;
 import net.mindengine.jeremy.objects.MyRemoteInterface;
+import net.mindengine.jeremy.objects.SerialObject;
 import net.mindengine.jeremy.registry.Lookup;
 import net.mindengine.jeremy.registry.Registry;
 import net.mindengine.jeremy.registry.RemoteMethodMetadata;
@@ -37,6 +41,7 @@ public class RegistryIntegrationTest {
     
     private static final String url = "http://localhost:8085";
     
+    private static Lookup lookup = new Lookup(url);
     @BeforeClass
     public static void initializeRegistry() throws InterruptedException {
         registryStarter = new RegistryStarter();
@@ -49,6 +54,9 @@ public class RegistryIntegrationTest {
         registryStarter.setRegistry(registry);
         registryStarter.startRegistry();
         Thread.sleep(2000);
+        
+        lookup = new Lookup(url);
+        
     }
     
     @AfterClass
@@ -134,7 +142,6 @@ public class RegistryIntegrationTest {
     
     @Test
     public void shouldInvokeRemoteMethods() throws RemoteObjectIsNotFoundException, ConnectionError {
-        Lookup lookup = new Lookup(url);
         
         //Creating proxy for remote object
         MyRemoteInterface remoteInterface = lookup.getRemoteObject("myObject", MyRemoteInterface.class);
@@ -145,6 +152,32 @@ public class RegistryIntegrationTest {
         //Checking that the previous method was implemented properly 
         String name = remoteInterface.getName();
         assertEquals("qwe", name);
+    }
+    
+    @Test
+    public void shouldSendComplexObjectAlongWithRemoteMethodInvocation() throws RemoteObjectIsNotFoundException, ConnectionError {
+      //Creating proxy for remote object
+        MyRemoteInterface remoteInterface = lookup.getRemoteObject("myObject", MyRemoteInterface.class);
+        
+        //Invoking remote method.
+        SerialObject object = new SerialObject();
+        object.setDate(new Date(12345678));
+        object.setDoubleField(34.056);
+        object.setLongField(234L);
+        object.setFloatField(12.03f);
+        object.setStringField("test object");
+        remoteInterface.setSerialObject(object);
+        
+        
+        SerialObject obj2 = remoteInterface.getSerialObject();
+        
+        assertNotNull(obj2);
+        assertEquals(object.getDate(), obj2.getDate());
+        assertEquals(object.getDoubleField(), obj2.getDoubleField());
+        assertEquals(object.getLongField(), obj2.getLongField());
+        assertEquals(object.getFloatField(), obj2.getFloatField());
+        assertEquals(object.getStringField(), obj2.getStringField());
+        
     }
     
     private static void assertContains(Object[]array, Object value) {
